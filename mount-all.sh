7becede6afb997bb67dbea196821209ad38dd397
Mike Orozco <mike.orozco@monrovia.com>
2019-07-11 07:25:44 -0700

    Be sure mount points exist before trying to mount.

--- mount-all.sh
+++ mount-all.sh
@@ -2,5 +2,8 @@
 for f in /fuse-config/conf.d/*; do
         filename="${f##*/}"
         basename="${filename%%.[^.]*}"
+        mkdir -p "/mnt/fuse-${basename}"
+        mkdir -p "/mnt/fusetmp-${basename}"
+        chown www-data:www-data /mnt/fuse*
         /usr/bin/blobfuse "/mnt/fuse-${basename}" --tmp-path="/mnt/fusetmp-${basename}" -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --config-file="/fuse-config/conf.d/${filename}"
 done
