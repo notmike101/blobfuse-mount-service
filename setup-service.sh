f3838373fdf6d04a36d65d31896e9a3d0ce04bcf
Mike Orozco <mike.orozco@monrovia.com>
2019-07-11 10:52:31 -0700

    Add service-setup.sh Runs before service actually starts

---
+++ setup-service.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for f in /fuse-config/conf.d/*; do
+    filename="${f##*/}"
+    basename="${filename%%.[^.]*}"
+    mkdir -p "/mnt/fuse-${basename}"
+    chown www-data:www-data "/mnt/fuse-${basename}"
+    mkdir -p "/mnt/fusetmp-${basename}"
+    chown www-data:www-data "/mnt/fusetmp-${basename}"
+done
