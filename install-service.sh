518567609743f7d16b4e59c6a518dd22e8056eda
Mike Orozco <mike.orozco@monrovia.com>
2019-07-11 11:02:07 -0700

    Force install service if it's already installed

--- install-service.sh
+++ install-service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cp fuse-mount.service /lib/systemd/system/fuse-mount.service
-ln -s /lib/systemd/system/fuse-mount.service /etc/systemd/system/fuse-mount.service
+ln -sf /lib/systemd/system/fuse-mount.service /etc/systemd/system/fuse-mount.service
 systemctl enable fuse-mount.service
